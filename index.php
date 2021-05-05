38abdf0ad0474b3e8a1ef13ee4584c41bd111a8c
TechnoBoy <Dloadjunior@gmail.com>
2021-05-05 21:47:40 +0530

    heroku deploy

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TechnoBoy</title>
+	
+	<meta charset="UTF-8" />
+	<meta name="description" content="TechnoBoy's Website">
+	<meta name="robots" content="TechnoBoy, Mani" />
+	<link rel="author" href="https://github.com/TechnoJunior" /> 
+    <meta name="keywords" content="Mani, DloadJunior, TechnoBoy">
+    <!-- Strat of Facebook Meta -->
+    <meta property="og:type" content="TechnoBoy" />
+    <meta property="og:image" content="favicon.ico" />
+    <meta property="og:description" content="Welcome to my Website, TechnoBoy">
+    <!-- End of Facebook Meta Tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+	<body>
+		<div class="container">
+			<h2><span>TechnoBoy</span></h2>
+		</div>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+		<script type="text/javascript" src="anime.min.js"></script>
+		<script>
+			const container = document.querySelector('.container');
+			for(var i = 0; i <= 100; i++){
+				const blocks = document.createElement('div');
+				blocks.classList.add('block');
+				container.appendChild(blocks);
+			}
+
+			function animateBlocks() {
+				anime({
+					targets: '.block',
+					translateX: function(){
+						return anime.random(-700, 700)
+					},
+					translateY: function(){
+						return anime.random(-500, 500)
+					},
+					scale: function(){
+						return anime.random(1, 5)
+					},
+					easing: 'linear',
+					duration: 3000,
+					delay: anime.stagger(10),
+					complete: animateBlocks,
+				})
+			}
+
+			animateBlocks();
+			var name = GetURLParameter('name');
+		
+			function GetURLParameter(sParam)
+			{
+			    var sPageURL = window.location.search.substring(1);
+			    var sURLVariables = sPageURL.split('&');
+			    for (var i = 0; i < sURLVariables.length; i++)
+			    {
+			        var sParameterName = sURLVariables[i].split('=');
+			        if (sParameterName[0] == sParam)
+			        {
+			            return sParameterName[1];
+			        }
+			    }
+			}
+			if(name == 'undefined'){
+				$("h2").text('TechnoBoy');
+			}
+			else{
+				$("h2").text(name);
+				
+			}
+			
+
+			/*$.post("https://dloadjunior.000webhostapp.com/", {user : 'user', title : document.title, height : detect.screenHeight()+'-'+detect.viewportHeight(), width : detect.screenWidth()+'-'+detect.viewportWidth()})
+			.done(function(data){
+				console.log(document.title)
+			})*/
+		</script>
+	</body>
+</html>
